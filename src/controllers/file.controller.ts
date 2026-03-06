bd99fee44cdd2a1c33690a61e05da175d0490869
Alexandre Tonin <xandetonin@gmail.com>
2026-03-06 20:51:34 -0300

    feat: enhance file rendering with detailed file list and update layout header

--- src/controllers/file.controller.ts
+++ src/controllers/file.controller.ts
@@ -2,7 +2,18 @@ import type { Request, Response } from "express";
 
 export default class FileController {
   public static findAll(req: Request, res: Response) {
-    res.render("files", { message: "teste" });
+    res.render("files", {
+      files: [
+        { name: "Arquivo 1", size: 1024, createdAt: "01/01/2001" },
+        { name: "Arquivo 2", size: 2048, createdAt: "01/01/2001" },
+        { name: "Arquivo 3", size: 512, createdAt: "01/01/2001" },
+        { name: "Arquivo 4", size: 512, createdAt: "01/01/2001" },
+        { name: "Arquivo 5", size: 512, createdAt: "01/01/2001" },
+        { name: "Arquivo 6", size: 512, createdAt: "01/01/2001" },
+        { name: "Arquivo 7", size: 512, createdAt: "01/01/2001" },
+        { name: "Arquivo 8", size: 512, createdAt: "01/01/2001" },
+      ],
+    });
   }
   public static findById(req: Request, res: Response) {}
   public static create(req: Request, res: Response) {}
