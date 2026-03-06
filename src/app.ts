2bf49eceb2a345c209489c2c4e401a9b459f7712
Alexandre Tonin <xandetonin@gmail.com>
2026-03-06 19:59:26 -0300

    feat: initialize nodejs project and setup ts, express and handlebars

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import { engine } from "express-handlebars";
+import morgan from "morgan";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(morgan("dev"));
+
+app.engine(
+  ".hbs",
+  engine({
+    extname: ".hbs",
+    defaultLayout: "main",
+    layoutsDir: path.join(__dirname, "views", "layouts"),
+  }),
+);
+
+app.set("view engine", ".hbs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (_req, res) => {
+  res.render("home", {
+    message: "teste",
+  });
+});
